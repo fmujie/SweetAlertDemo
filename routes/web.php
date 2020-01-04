c41d487a6e7c10601f004610a63ab9a8569f31d8
Fangjx <1935377905@qq.com>
2020-01-04 18:38:53 +0800

    resetroute

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/SweetAlert/{alertType?}', 'SweetAlertDemo@index')->middleware('adminAuth');
+// Route::get('/SweetAlert/{alertType?}', 'SweetAlertDemo@index')->middleware('adminAuth');
+
+Route::get('/SweetAlert/{alertType?}', 'SweetAlertDemo@index');
